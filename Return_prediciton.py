adba6052fc9d0936b3b4ab88a720d8bb6315345b
KUGALUR PALANISAMY Nithyaraaj <141884467+Nithyaraaj21@users.noreply.github.com>
2024-04-12 16:12:15 +0200

    Update Return_prediciton.py

--- Return_prediciton.py
+++ Return_prediciton.py
@@ -5,24 +5,21 @@ from category_encoders import OrdinalEncoder
 import pickle
 
 # Load the dataset
-# Specify the compressed file name
 compressed_file_name = "./converted_dataset.csv.gz"
-
-# Read the compressed CSV file directly with pd.read_csv
 data = pd.read_csv(compressed_file_name, compression='gzip')
 
 # Prepare data for predictive modeling
-X = data.drop(columns=["Returned", "SaleDocumentNumber", "CustomerID"])
+X = data[['Shop','BrandName', 'ModelGroup', 'ProductGroup']]
 y = data['Returned']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Preprocess categorical features using Ordinal Encoder
-categorical_columns = ['BrandName', 'ModelGroup', 'ProductGroup', 'Shop']
+categorical_columns = ['Shop','BrandName', 'ModelGroup', 'ProductGroup']
 ordinal_encoder = OrdinalEncoder()
 X_train_encoded = ordinal_encoder.fit_transform(X_train[categorical_columns])
 X_test_encoded = ordinal_encoder.transform(X_test[categorical_columns])
 
-# Train the model (example: Decision Tree Classifier)
+# Train the model (example: Random Forest Classifier)
 model = DecisionTreeClassifier(random_state=42)
 model.fit(X_train_encoded, y_train)
 
@@ -33,5 +30,39 @@ model_data = {
 }
 
 # Save the serialized objects to a pickle file
-with open('model_data.pkl', 'wb') as file:
+with open('model_data_dt.pkl', 'wb') as file:
     pickle.dump(model_data, file)
+
+# Load the trained model and ordinal encoder from the pickle file
+with open('model_data_dt.pkl', 'rb') as file:
+    model_data = pickle.load(file)
+
+# Extract the model and ordinal encoder from the loaded data
+model = model_data['model']
+ordinal_encoder = model_data['ordinal_encoder']
+
+# Check column names of input data
+print("Input Data Columns:")
+print(X_test.columns)
+
+# Check feature names used during training
+print("Feature Names Used During Training:")
+print(X_train_encoded.columns)
+
+# Check categories mapped by the Ordinal Encoder
+print("Categories Mapped by the Ordinal Encoder:")
+for col, mapping in zip(categorical_columns, ordinal_encoder.category_mapping):
+    print(f"{col}: {mapping}")
+   
+
+# Ensure number of columns is consistent between training and prediction datasets
+if len(X_train_encoded.columns) == len(X_test_encoded.columns):
+    print("Number of columns is consistent between training and prediction datasets.")
+else:
+    print("Number of columns is not consistent between training and prediction datasets.")
+
+# Ensure column names are consistent between training and prediction datasets
+if all(X_train_encoded.columns == X_test_encoded.columns):
+    print("Column names are consistent between training and prediction datasets.")
+else:
+    print("Column names are not consistent between training and prediction datasets.")
