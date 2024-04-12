2a1f63d09e3d15a17712eb9c76425d1dfb8681eb
KUGALUR PALANISAMY Nithyaraaj <141884467+Nithyaraaj21@users.noreply.github.com>
2024-04-12 16:12:43 +0200

    Update Return_predicition_app.py

--- Return_predicition_app.py
+++ Return_predicition_app.py
@@ -3,9 +3,10 @@ import pandas as pd
 import pickle
 import gzip
 import io
+import numpy as np  # Import NumPy for array manipulation
 
 # Load the pickled model and preprocessing steps
-with open('model_data.pkl', 'rb') as file:
+with open('model_data_dt.pkl', 'rb') as file:
     model_data = pickle.load(file)
 
 # Extract the model and ordinal encoder from the loaded data
@@ -22,10 +23,18 @@ with gzip.open(compressed_file_name, "rb") as f:
     # Read the CSV data from the file-like object
     data = pd.read_csv(compressed_file_object)
 
+# Define categorical columns
+categorical_columns = ['Shop', 'BrandName', 'ModelGroup', 'ProductGroup']
+
 # Define a function to preprocess input data and make predictions
 def predict_return(data):
+    # Reorder columns to match the order used during training
+    data_reordered = data[['Shop', 'BrandName', 'ModelGroup', 'ProductGroup']]
     # Preprocess the input data using the ordinal encoder
-    data_encoded = ordinal_encoder.transform(data)
+    data_encoded = ordinal_encoder.transform(data_reordered)
+    # Reshape the data to match the expected input shape of the model
+    data_encoded = np.array(data_encoded)  # Convert to NumPy array
+    data_encoded = data_encoded.reshape(1, -1)  # Reshape to a single row
     # Make predictions using the trained model
     predictions = model.predict(data_encoded)
     return predictions
@@ -39,9 +48,8 @@ def main():
     st.header('Enter Customer Data')
 
     # Create dropdown menus for the specified columns
-    columns_to_select = ['BrandName', 'ModelGroup', 'ProductGroup']
     selected_values = []
-    for column in columns_to_select:
+    for column in categorical_columns:
         # Use unique values in the column as options for the dropdown menu
         options = data[column].unique()
         # Add a dropdown menu for the current column
@@ -51,7 +59,7 @@ def main():
     # Button to make predictions
     if st.button('Predict'):
         # Convert selected values to DataFrame
-        input_data = {column: [selected_value] for column, selected_value in zip(columns_to_select, selected_values)}
+        input_data = {column: [selected_value] for column, selected_value in zip(categorical_columns, selected_values)}
         input_df = pd.DataFrame(input_data)
 
         # Preprocess input data and make predictions
